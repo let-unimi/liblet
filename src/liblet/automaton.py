34b4418eda8b281f8e19cf78ea51a49cd1596668
Massimo Santini <massimo.santini@gmail.com>
2019-04-02 01:44:54 +0200

    Added test to reach 100% coverage and started to document and typecheck

--- src/liblet/automaton.py
+++ src/liblet/automaton.py
@@ -1,37 +1,93 @@
 from collections import namedtuple
+from collections.abc import Set
+from functools import total_ordering
 from operator import attrgetter
 
 from . import DIAMOND, ε
 from .utils import letstr
 
-Transition = namedtuple('Transition', 'frm label to')
-"""An automaton transition.
 
-    This class represents an automaton transition; it has a `frm` starting
-    *state* and a `to` destination *state* and a `label`, the states can be
-    *nonempty* :obj:`strings <str>` or *nonempty* :obj:`sets <set>` of
-    *nonempty* strings, whereas the label is usually a string.  A transition is
-    :term:`iterable` and unpacking can be used to obtain its components, so for
-    example
 
-    .. doctest::
+@total_ordering
+class Transition:
+    """An automaton transition.
 
-        >>> frm, label, to = Transition({'A', 'B'}, 'c', {'D'})
-        >>> frm
-        {'B', 'A'}
-        >>> label
-        'c'
-        >>> to
-        {'D'}
+        This class represents an automaton transition; it has a `frm` starting
+        *state* and a `to` destination *state* and a `label`, the states can be
+        *nonempty* :obj:`strings <str>` or *nonempty* :obj:`sets <set>` of
+        *nonempty* strings, whereas the label is usually a string.  A transition is
+        :term:`iterable` and unpacking can be used to obtain its components, so for
+        example
 
-    Args: 
-        frm (:obj:`str` or :obj:`set` of :obj:`str`): The starting state(s) of the transition. 
-        label (:obj:`str`): The label of the transition.
-        to (:obj:`str` or :obj:`set` of :obj:`str`): The destination state(s) of the transition.
+        .. doctest::
 
-"""
+            >>> frm, label, to = Transition({'A', 'B'}, 'c', {'D'})
+            >>> frm
+            {'B', 'A'}
+            >>> label
+            'c'
+            >>> to
+            {'D'}
 
-Transition.__repr__ = lambda self: '{}-{}->{}'.format(letstr(self.frm), self.label, letstr(self.to))
+        Args: 
+            frm (:obj:`str` or :obj:`set` of :obj:`str`): The starting state(s) of the transition. 
+            label (str): The label of the transition.
+            to (:obj:`str` or :obj:`set` of :obj:`str`): The destination state(s) of the transition.
+
+        Raises:
+            ValueError: in case the frm or to states are not nonempty strings, or nonempty set 
+                        of nonempty strings, or the label is not a nonempty string.
+    """
+
+    __slots__ = ('frm', 'label', 'to')
+
+    def __init__(self, frm, label, to):
+
+        def _cssos(s):
+            if isinstance(s, str) and s: return True
+            if isinstance(s, Set) and s and all(map(lambda _: isinstance(_, str) and _, s)): return True
+            return False
+
+        if _cssos(frm):
+            self.frm = frm
+        else:
+            raise ValueError('The frm state is not a nonempty string, or a nonempty set of nonempty strings')
+        if isinstance(label, str) and label:
+            self.label = label
+        else:
+            raise ValueError('The label is not a nonempty string')
+        if _cssos(to):
+            self.to = to
+        else:
+            raise ValueError('The to state is not a nonempty string, or a nonempty set of nonempty strings')
+        self.to = to
+
+    def __lt__(self, other):
+        if not isinstance(other, Transition): return NotImplemented
+        return (self.frm, self.label, self.to) < (other.frm, other.label, other.to)
+        
+    def __eq__(self, other):
+        if not isinstance(other, Transition): return False
+        return (self.frm, self.label, self.to) == (other.frm, other.label, other.to)
+
+    def __hash__(self):
+        return hash((self.frm, self.label, self.to))
+
+    def __iter__(self):
+        return iter((self.frm, self.label, self.to))
+
+    def __repr__(self):
+        return '{}-{}->{}'.format(letstr(self.frm), self.label, letstr(self.to))
+
+    @classmethod
+    def from_string(cls, transitions):
+        res = []
+        for t in transitions.splitlines():
+            if not t.strip(): continue
+            frm, label, to = t.split(',')
+            res.append(Transition(frm.strip(), label.strip(), to.strip()))
+        return res
+
 
 class Automaton(object):
     """An automaton.
@@ -39,52 +95,63 @@ class Automaton(object):
         This class represents a (*nondeterministic*) *finite automaton*.
 
         Args: 
-            N (:obj:`set`): The states of the automaton. 
-            T (:obj:`set`): The transition labels.
+            N (set): The states of the automaton. 
+            T (set): The transition labels.
             transitions (:obj:`set` of :obj:`Transition`): The transition of the automata.
-            F (:obj:`set`): The set of *final* states.
             q0: The starting state of the automaton.
+            F (set): The set of *final* states.
     """
+
+    __slots__ = ('N', 'T', 'transitions', 'q0', 'F')
     
-    def __init__(self, N, T, transitions, F, q0):
+    def __init__(self, N, T, transitions, q0, F):
         self.N = set(N)
         self.T = set(T)
         self.transitions = tuple(transitions)
-        self.F = set(F)
         self.q0 = q0
+        self.F = set(F)
+        if self.N & self.T: raise ValueError('The set of states and input symbols are not disjoint, but have {} in common.'.format(letstr(set(self.N & self.T))))
+        if not self.q0 in self.N: raise ValueError('The specified q0 ({}) is not a state.'.format(letstr(q0)))    
+        if not self.F <= self.N: raise ValueError('The accepting states {} in F are not states.'.format(letstr(self.F - self.N)))        
+        bad_trans = tuple(t for t in transitions if not t.frm in self.N or not t.to in self.N or not t.label in (self.T | {ε}))
+        if bad_trans: raise ValueError('The following transitions contain states or symbols that are neither states nor input symbols: {}.'.format(bad_trans))
 
     def δ(self, X, x):
+        """The transition function.
+
+        This function returns the set of states reachable from the given state and input symbol.
+
+        Args:
+            X: the state.
+            x: the input symbol.
+        Returns:
+            The set of next states of the automaton.
+        """
         return {Z for Y, y, Z in self.transitions if Y == X and y == x}
 
     def __repr__(self):
         return 'Automaton(N={}, T={}, transitions={}, F={}, q0={})'.format(letstr(self.N), letstr(self.T), self.transitions, letstr(self.F), letstr(self.q0))
    
     @classmethod
-    def from_transitions(cls, transitions, F = None, q0 = None):
-        transitions = tuple(map(lambda _: Transition(*_), transitions))
+    def from_string(cls, transitions, F = None, q0 = None):
+        transitions = Transition.from_string(transitions)
+        if F is None: F = set()
         if q0 is None: q0 = transitions[0].frm
-        if F is None:  F = set()
         N = set(map(attrgetter('frm'), transitions)) | set(map(attrgetter('to'), transitions))
         T = set(map(attrgetter('label'), transitions)) - {ε}
-        return cls(N, T, transitions, F, q0)
+        return cls(N, T, transitions, q0, F)
 
     @classmethod
     def from_grammar(cls, G):
         res = []
         for P in G.P:
+            if len(P.rhs) > 2: raise ValueError('Production {} has more than two symbols on the lefthand side'.format(P))
             if len(P.rhs) == 2:
-                res.append(Transition(*((P.lhs, ) + P.rhs)))
+                A, (a, B) = P
+                if not (a in G.T and B in G.N): raise ValueError('Production {} right hand side is not of the aB form'.format(P))
+                res.append(Transition(A, a, B))
             elif P.rhs[0] in G.N:
                 res.append(Transition(*((P.lhs, ) + (ε, ) + P.rhs)))
             else:
                 res.append(Transition(*((P.lhs, ) + P.rhs + (DIAMOND,))))
-        return cls(G.N | {DIAMOND}, G.T, tuple(res), {DIAMOND}, G.S)
-
-    @classmethod
-    def from_string(cls, trans, F = None):
-        transitions = []
-        for t in trans.splitlines():
-            if not t.strip(): continue
-            frm, label, to = t.split(',')
-            transitions.append(Transition(frm.strip(), label.strip(), to.strip()))
-        return Automaton.from_transitions(transitions, set() if F is None else F)
+        return cls(G.N | {DIAMOND}, G.T, tuple(res), G.S, {DIAMOND})
--- src/tests/automaton_test.py
+++ src/tests/automaton_test.py
@@ -1,9 +1,78 @@
 import unittest
 
-from liblet import Automaton, Grammar
+from liblet import Automaton, Transition, Grammar
 
 
 class TestAutomaton(unittest.TestCase):
+
+    def test_transition_unpack(self):
+        f, l, t = Transition('a', 'b', 'c')
+        self.assertEqual(('a', 'b', 'c'), (f, l, t))
+
+    def test_transition_totalorder(self):
+        self.assertTrue(Transition('a', 'b', 'c') > Transition('a', 'b', 'b'))
+
+    def test_transition_eqo(self):
+        self.assertFalse(Transition('a', 'b', 'c') == object())
+
+    def test_transition_lto(self):
+        self.assertIs(Transition('a', 'b', 'c').__lt__(object()), NotImplemented)
+
+    def test_transition_hash(self):
+        S = {
+            Transition('a', 'b', 'c'): 1,
+            Transition('a', 'b', 'c'): 2
+        }
+        self.assertEqual(1, len(S))
+
+    def test_transition_str(self):
+        self.assertEqual('frm-label->to', str(Transition('frm', 'label', 'to')))
+
+    def test_transition_lt(self):
+        self.assertEqual('frm-label->to', str(Transition('frm', 'label', 'to')))
+
+    def test_transition_set(self):
+        self.assertEqual('{frm}-label->{to}', str(Transition({'frm'}, 'label', {'to'})))
+
+    def test_transition_wrong_label1(self):
+        with self.assertRaisesRegex(ValueError, 'The label is not'):
+            Transition('frm', 1, 'to')
+
+    def test_transition_wrong_label2(self):
+        with self.assertRaisesRegex(ValueError, 'The label is not'):
+            Transition('frm', '', 'to')
+
+    def test_transition_wrong_from1(self):
+        with self.assertRaisesRegex(ValueError, 'The frm state is not'):
+            Transition(1, 'label', 'to')
+
+    def test_transition_wrong_from2(self):
+        with self.assertRaisesRegex(ValueError, 'The frm state is not'):
+            Transition('', 'label', 'to')
+
+    def test_transition_wrong_from3(self):
+        with self.assertRaisesRegex(ValueError, 'The frm state is not'):
+            Transition(set(), 'label', 'to')
+
+    def test_transition_wrong_from4(self):
+        with self.assertRaisesRegex(ValueError, 'The frm state is not'):
+            Transition({''}, 'label', 'to')
+
+    def test_transition_wrong_to1(self):
+        with self.assertRaisesRegex(ValueError, 'The to state is not'):
+            Transition('frm', 'label', 1)
+
+    def test_transition_wrong_to2(self):
+        with self.assertRaisesRegex(ValueError, 'The to state is not'):
+            Transition('frm', 'label', '')
+
+    def test_transition_wrong_to3(self):
+        with self.assertRaisesRegex(ValueError, 'The to state is not'):
+            Transition('frm', 'label', set())
+
+    def test_transition_wrong_to4(self):
+        with self.assertRaisesRegex(ValueError, 'The to state is not'):
+            Transition('frm', 'label', {''})
 
     def test_automaton_from_grammar(self):
         # fig 5.6, pag 142
@@ -18,6 +87,31 @@ class TestAutomaton(unittest.TestCase):
         """))
         s = 'Automaton(N={A, B, C, S, ◇}, T={a, b, c}, transitions=(S-a->A, S-a->B, A-b->B, A-b->C, B-c->A, B-c->C, C-a->◇), F={◇}, q0=S)'
         self.assertEqual(s, str(A))
+
+    def test_automaton_δ(self):
+        # fig 5.6, pag 142
+        states = Automaton.from_grammar(Grammar.from_string("""
+            S -> a A
+            S -> a B
+            A -> b B
+            A -> b C
+            B -> c A
+            B -> c C
+            C -> a
+        """)).δ('S', 'a')
+        self.assertEqual({'A', 'B'}, states)
+
+    def test_automaton_from_grammar_fail3(self):
+        with self.assertRaisesRegex(ValueError, 'has more than two symbols'):
+            Automaton.from_grammar(Grammar.from_string('S -> a b c'))
+
+    def test_automaton_from_grammar_fail2a(self):
+        with self.assertRaisesRegex(ValueError, 'not of the aB form'):
+            Automaton.from_grammar(Grammar.from_string('S -> a b'))
+
+    def test_automaton_from_grammar_fail2b(self):
+        with self.assertRaisesRegex(ValueError, 'not of the aB form'):
+            Automaton.from_grammar(Grammar.from_string('S -> B B'))
 
     def test_automaton_from_ε_grammar(self):
         # fig 5.14 pag 147
@@ -52,5 +146,17 @@ class TestAutomaton(unittest.TestCase):
             """, {'C'})
         s = 'Automaton(N={A, B, C, D, E, F, G, H}, T={0, 1}, transitions=(A-0->B, A-1->F, B-0->G, B-1->C, C-1->C, D-0->C, D-1->G, E-0->H, E-1->F, F-0->C, F-1->G, G-0->G, H-0->G, H-1->C), F={C}, q0=A)'
         self.assertEqual(s, str(A))
+
+    def test_automaton_overlapTN(self):
+        with self.assertRaisesRegex(ValueError, "but have {B} in common"):
+            Automaton({'A', 'B'}, {'B', 'C'}, tuple(), set(), 'A')
+
+    def test_automaton_q0N(self):
+        with self.assertRaisesRegex(ValueError, r'\(X\) is not a state'):
+            Automaton({'A', 'B'}, {'b', 'c'}, tuple(), 'X', set())
+
+    def test_automaton_FN(self):
+        with self.assertRaisesRegex(ValueError, r'states {C} in F are not states'):
+            Automaton({'A', 'B'}, {'b', 'c'}, tuple(), 'A', {'B', 'C'})
 
 if __name__ == '__main__': unittest.main()
