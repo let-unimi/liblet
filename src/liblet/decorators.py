aceeaffc895e3a4caad328d359ffe5c7e3009e33
Massimo Santini <massimo.santini@gmail.com>
2020-05-14 22:02:37 +0200

    Closure returns the computed value, to ensure same type as wrapped.

--- src/liblet/decorators.py
+++ src/liblet/decorators.py
@@ -12,8 +12,8 @@ def closure(f):
         f: the function to wrap in the closure computation.
 
     Example:
-        
-        Consider the following example where the function ``f`` given a set ``S`` of integers and 
+
+        Consider the following example where the function ``f`` given a set ``S`` of integers and
         an integer ``m`` as input, returns the set obtained adding to ``S`` all the values
         ``s - 1`` for all its elements greater than some lower limit ``m``.
 
@@ -22,7 +22,7 @@ def closure(f):
             >>> @closure
             ... def reduce_up_to(S, m):
             ...     return S | {s - 1 for s in S if s > m}
-            ... 
+            ...
             >>> reduce_up_to({7,5}, 3)
             {3, 4, 5, 6, 7}
 
@@ -30,15 +30,15 @@ def closure(f):
 
     Notes:
 
-        More formally, consider a function :math:`f : \mathfrak{D}\\times \mathfrak{X} \\to \mathfrak{D}` 
-        where :math:`\mathfrak{D}` is a domain of interest and  :math:`\mathfrak{X}` is the (possibly 
+        More formally, consider a function :math:`f : \mathfrak{D}\\times \mathfrak{X} \\to \mathfrak{D}`
+        where :math:`\mathfrak{D}` is a domain of interest and  :math:`\mathfrak{X}` is the (possibly
         empty) domain of extra parameters. Define as usual the iterated application of :math:`f` as
         :math:`f^{(n + 1)}(D, X) = f(f^{(n)}(D, X), X)`, where :math:`f^{(0)}(D, X) = f(D, X)`. Then
-        :math:`F = \\textit{closure}(f)` is the function 
-        :math:`F: \mathfrak{D}\\times \mathfrak{X} \\to \mathfrak{D}`  defined as 
+        :math:`F = \\textit{closure}(f)` is the function
+        :math:`F: \mathfrak{D}\\times \mathfrak{X} \\to \mathfrak{D}`  defined as
         :math:`F(D, X) = f^{(\hat{n})}(D, X)` where :math:`\hat{n}` is the minimum
-        :math:`n` (depending on :math:`D` and :math:`X`) such that 
-        :math:`f^{(\hat{n} + 1)}(D, X) = f^{(\hat{n})}(D, X)`.  
+        :math:`n` (depending on :math:`D` and :math:`X`) such that
+        :math:`f^{(\hat{n} + 1)}(D, X) = f^{(\hat{n})}(D, X)`.
 
     """
     @wraps(f)
@@ -46,14 +46,14 @@ def closure(f):
         s, *other = args
         while True:
             n = f(deepcopy(s), *other)
-            if n == s: return s
+            if n == s: return n
             s = n
     return _closure
 
 def show_calls(show_retval = False):
     """Wraps a function so that it calls (and return values) are printed when invoked.
 
-    This decorator takes a function a decorated function that prints every invocation 
+    This decorator takes a function a decorated function that prints every invocation
     (with the value of the parameters and optionally the returned value).
 
     Args:
@@ -62,16 +62,16 @@ def show_calls(show_retval = False):
 
     Example:
 
-        This is how to use the decorator with the classic recursive implementation of 
+        This is how to use the decorator with the classic recursive implementation of
         the Fibonacci numbers computation.
-        
+
         .. doctest::
 
             >>> @show_calls(True)
             ... def fib(n):
             ...     if n == 0 or n == 1: return 1
             ...     return fib(n - 1) + fib(n - 2)
-            ... 
+            ...
             >>> _ = fib(3)
             ┌fib(3)
             │┌fib(2)
